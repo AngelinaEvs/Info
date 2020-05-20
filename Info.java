22f317ad454d8de1de0d6525c35d6178ed76cdf2
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-05-20 18:22:04 +0300

    Info

---
+++ Info.java
@@ -0,0 +1,8 @@
+//Java
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.printf("%19s%4s%s%n", "Цель:", " ", "хочу стать хорошим программистом");
+        System.out.printf("%19s%4s%s%n", "Использовала языки:", " ", "Java, C#, Python, Pascal, HTML, CSS");
+    }
+}
---
+++ Info.py
@@ -0,0 +1,4 @@
+s1 = '{x}{y}{z}'
+s2 = '{a}{b}{c}'
+print(s1.format(x = 14 * " " + "Цель:", y = 4 * " ", z = "стать хорошим программистом"))
+print(s2.format(a = "Использовала языки:", b = 4 * " ", c = "Java, C#, Python, Pascal, HTML, CSS"))
